83ea05affb791284c3dc136bc532842469d96f46
João Dias <mail@joaodias.eu>
2017-11-17 12:22:30 +0100

    make lazy route virtual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
   },
   {
     path: 'lazy',
-    loadChildren: './lazy/lazy.module#LazyModule',
+    loadChildren: 'xc-af/app/lazy/lazy.module#LazyModule',
   },
 ];
 
